a5dde385175b858b18d3d2b4db674081177163ac
Artem Fedulov <arfedulov@gmail.com>
2023-11-07 23:11:43 +0300

    add mocks

--- src/api/api.js
+++ src/api/api.js
@@ -11,8 +11,35 @@ const mockConfigurations = {
 }
 const mockModels = {
     'standard+six': [
-        { id: 'one', label: 'One' }
-    ]
+        { id: 'standardSix01', label: 'Standard Six 01' },
+        { id: 'standardSix02', label: 'Standard Six 02' },
+        { id: 'standardSix03', label: 'Standard Six 03' },
+    ],
+    'standard+eight': [
+        { id: 'standardEight01', label: 'Standard Eight 01' },
+        { id: 'standardEight02', label: 'Standard Eight 02' },
+        { id: 'standardEight03', label: 'Standard Eight 03' },
+    ],
+    'standard+twelve': [
+        { id: 'standardTwelve01', label: 'Standard Twelve 01' },
+        { id: 'standardTwelve02', label: 'Standard Twelve 02' },
+        { id: 'standardTwelve03', label: 'Standard Twelve 03' },
+    ],
+    'extended+six': [
+        { id: 'extendedSix01', label: 'Extended Six 01' },
+        { id: 'extendedSix02', label: 'Extended Six 02' },
+        { id: 'extendedSix03', label: 'Extended Six 03' },
+    ],
+    'extended+eight': [
+        { id: 'extendedEight01', label: 'Extended Eight 01' },
+        { id: 'extendedEight02', label: 'Extended Eight 02' },
+        { id: 'extendedEight03', label: 'Extended Eight 03' },
+    ],
+    'extended+twelve': [
+        { id: 'extendedTwelve01', label: 'Extended Twelve 01' },
+        { id: 'extendedTwelve02', label: 'Extended Twelve 02' },
+        { id: 'extendedTwelve03', label: 'Extended Twelve 03' },
+    ],
 }
 
 const baseUrl = new URL(window.location.href).origin
